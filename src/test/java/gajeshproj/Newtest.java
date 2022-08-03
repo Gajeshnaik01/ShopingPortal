7fb13c098e264ead7a97380d3b4398f88fbc757f
Pmani498 <mani.palluru@gmail.com>
2022-08-03 12:07:54 +0530

    second commit

--- src/test/java/gajeshproj/Newtest.java
+++ src/test/java/gajeshproj/Newtest.java
@@ -5,6 +5,7 @@ public class Newtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Trial project for git");
+		System.out.println("Trial project for git");
 		
 		
 	}
