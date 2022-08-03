0aa3baa10bbbcff563d9895ce00dc37b944fce51
Pmani498 <mani.palluru@gmail.com>
2022-08-03 12:01:06 +0530

    Mani commited the code

--- src/test/java/gajeshproj/Newtest.java
+++ src/test/java/gajeshproj/Newtest.java
@@ -4,8 +4,8 @@ public class Newtest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Trial project fo rgit");
-		System.out.println("Trial project fo rgit");
+		System.out.println("Trial project for git");
+		
 		
 	}
 
