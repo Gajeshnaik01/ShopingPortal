1df28c797af19c2f13267ff2056dd0aff5ed235c
Gajeshnaik <gajesh.naik1413@gmail.com>
2022-08-03 10:34:29 +0530

    commit new

---
+++ src/test/java/org/github/pratice/PraticeTest1.java
@@ -0,0 +1,34 @@
+package org.github.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest1 {
+	
+
+	@Test
+	public void test1() {
+		String browser=System.getProperty("browser");
+		String url=System.getProperty("url");
+		String un=System.getProperty("USERNAME");
+		String pwd=System.getProperty("PASSWORD");
+		System.out.println("test1 --> PraticeTest1");
+		System.out.println("Browser --> "+browser);
+		System.out.println("URL --> "+url);
+		System.out.println("username --> "+un);
+		System.out.println("password --> "+pwd);
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+		String browser=System.getProperty("browser");
+		String url=System.getProperty("url");
+		System.out.println(browser);
+		System.out.println(url);
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/org/github/pratice/PraticeTest2.java
@@ -0,0 +1,29 @@
+package org.github.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest2 {
+	
+
+	@Test
+	public void test1() {
+		
+		System.out.println("test1 --> PraticeTest2");
+		System.out.println("test1 --> PraticeTest2");
+		
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("test2 --> PraticeTest2");
+		System.out.println("test2 --> PraticeTest2");
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("test3 --> PraticeTest2");
+	}
+	
+
+}
