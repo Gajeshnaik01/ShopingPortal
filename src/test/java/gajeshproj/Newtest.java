8419f4263b0e5568486dfbb4f7ae068dabbc2c70
Pmani498 <mani.palluru@gmail.com>
2022-08-03 11:07:02 +0530

    first commit

---
+++ src/test/java/gajeshproj/Newtest.java
@@ -0,0 +1,12 @@
+package gajeshproj;
+
+public class Newtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Trial project fo rgit");
+		System.out.println("Trial project fo rgit");
+		System.out.println("Trial project fo rgit");
+	}
+
+}
